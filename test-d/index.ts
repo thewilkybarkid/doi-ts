0ea87640727868a03aebbe852e61565e3dd8a6eb
Chris Wilkinson <c.wilkinson@elifesciences.org>
2022-07-12 10:04:13 +0100

    ci: simplify type tests
    Refs https://github.com/thewilkybarkid/uuid-ts/commit/db92a4b3c164998b47da6847c8885758842c2aa6

---
+++ test-d/index.ts
@@ -0,0 +1,37 @@
+import { expectTypeOf } from 'expect-type'
+import { Eq } from 'fp-ts/Eq'
+import * as O from 'fp-ts/Option'
+import { pipe } from 'fp-ts/function'
+import * as _ from '../src'
+
+import Doi = _.Doi
+import Option = O.Option
+
+declare const string: string
+declare const unknown: unknown
+declare const doi: Doi
+
+//
+// Doi
+//
+
+expectTypeOf(doi).toMatchTypeOf(string)
+expectTypeOf(string).not.toMatchTypeOf(doi)
+
+//
+// toUrl
+//
+
+expectTypeOf(_.toUrl(doi)).toEqualTypeOf<URL>()
+
+//
+// Eq
+//
+
+expectTypeOf(_.Eq).toEqualTypeOf<Eq<Doi>>()
+
+//
+// isDoi
+//
+
+expectTypeOf(pipe(unknown, O.fromPredicate(_.isDoi))).toEqualTypeOf<Option<Doi>>()
