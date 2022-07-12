da0b727573171d4325012b974806af3487c3ec89
Chris Wilkinson <c.wilkinson@elifesciences.org>
2022-07-12 09:33:47 +0100

    fix: allow 2-digit registrant codes
    Refs https://github.com/regexhq/doi-regex/pull/11

--- test/fc.ts
+++ test/fc.ts
@@ -7,7 +7,7 @@ export * from 'fast-check'
 export const doi = (): fc.Arbitrary<_.Doi> =>
   fc
     .tuple(
-      fc.stringOf(fc.constantFrom('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'), { minLength: 4 }),
+      fc.stringOf(fc.constantFrom('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'), { minLength: 2 }),
       fc.unicodeString({ minLength: 1 }),
     )
     .map(([prefix, suffix]) => `10.${prefix}/${suffix}` as _.Doi)
